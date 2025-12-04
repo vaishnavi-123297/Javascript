678dee97c8fd666aba295ea19d317a7211c6f3ef
vaishnavi-123297 <shevatevaishnavi@gmail.com>
2025-12-04 21:44:27 +0530

    Add various arithmetic and string operations

--- 01_BAICS/conversionoperation.js
+++ 01_BAICS/conversionoperation.js
@@ -37,4 +37,33 @@ console.log(booleanIsLoggedIn);//true
     // console.log(typeof stringNumber)
 }
 
+//----------oPERATION--------
+let value = 3
+let negValue = -value
+//console.log(negValue);//-3
+
+console.log(2+2);//4
+console.log("2"+"2");//22
+console.log("Hitesh"+"Chaudhary");//HiteshChaudhary
+console.log("2"+2);//22
+console.log(2-2)//0
+console.log(2*2);//4
+
+let str1 = "Hello"
+let str2 = "Hitesh"
+let str3 = str1 + " " + str2
+console.log(str3);//Hello Hitesh
+console.log("1"+ 1 + 1)//111
+console.log(1 + 1 + "1")//21
+console.log(3 + 4* 5%3)//5
+
+// console.log(+true)//1
+// console.log(="")//0
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+console.log(num1,num2,num3);//4 4 4
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);//101
+//prefix and postfix 
 

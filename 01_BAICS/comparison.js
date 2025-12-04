dadf4d367f12cdcf2e5f768126a55fa1a5dcc3c3
vaishnavi-123297 <shevatevaishnavi@gmail.com>
2025-12-04 22:09:52 +0530

    Add comparison examples in comparison.js

--- 01_BAICS/comparison.js
+++ 01_BAICS/comparison.js
@@ -1,1 +1,18 @@
+// console.log(2 == 1);
+// console.log(2 != 1)
+// console.log(2<1)
+// console.log(2>1)
 
+//At time of comparison if one is string and other is number then string convert to number and then comparison is done
+console.log("2">1);//true
+console.log("02"==2);
+
+console.log(null > 0);//false
+console.log(null == 0)//false
+console.log(null >= 0);//true
+//null is only equal to undefined
+
+
+//strict check ===
+console.log(2==="2");//false
+console.log(2=="2");//true

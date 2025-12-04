12dc75e8f18e6cf924cc9cb1fdc7c0550cd2cc30
vaishnavi-123297 <shevatevaishnavi@gmail.com>
2025-12-04 20:41:39 +0530

    Add examples of type conversion in JavaScript
    This code demonstrates type conversion in JavaScript, including converting between number and string types, handling null values, and using Boolean conversion.

--- 01_BAICS/conversionoperation.js
+++ 01_BAICS/conversionoperation.js
@@ -1,1 +1,40 @@
+{let score = 33
+console.log(typeof score);//number
+console.log(typeof(score));//number
+}
+let score = "33"
+console.log(typeof score);//string
+console.log(typeof(score));//string
+
+let valueinnumber =number(score)
+console.log(typeof valueinnumber);//convert string to number
+console.log( valueinnumber);//NaN
+
+{
+
+
+let score = null
+console.log(typeof score)// output is 0
+}
+//if value is true then 1
+//false is 0
+//if string "hitesh" convert to number it cant so output NaN
+//"33" => 33
+//"33abs" => NaN
+{
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);//true 
+}
+//1=> true  ;0=> false
+//"" => false 
+//"hitesh" => true 
+
+{
+    // let someNumber = 33
+    // let stringNumber =String(someNumber)
+    // console.log(stringNumber);
+    // console.log(typeof stringNumber)
+}
+
 

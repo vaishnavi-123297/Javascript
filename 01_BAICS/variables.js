1e6be4f7f325f3c005ef2e2abfab327a8893e3d7
vaishnavi-123297 <shevatevaishnavi@gmail.com>
2025-12-04 20:39:58 +0530

    Modify account variables and log output
    Updated account details and variable declarations.

--- 01_BAICS/variables.js
+++ 01_BAICS/variables.js
@@ -1,1 +1,17 @@
 
+const account_Id = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"//mostly not used due to scoped 
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2   //not allowed 
+accountEmail = "hc@gmail.com"
+accountPassword = 12333
+accountCity = "Banglore"
+console.log(accountId);
+/*
+prefer to not use var 
+because of issue in block scoped and function scoped  */
+console.table([accountEmail,accountPassword,account_Id,accountCity,accountState])
+//when value is not declared it gives undefined 
